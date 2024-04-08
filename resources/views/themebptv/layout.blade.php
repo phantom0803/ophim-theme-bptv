a7fc9cd9356c5ae921faee2711451a3c202a0c21
OPDLNF <opdlnf01@gmail.com>
2024-04-08 14:46:52 +0700

    fix syntax format

--- resources/views/themebptv/layout.blade.php
+++ resources/views/themebptv/layout.blade.php
@@ -8,35 +8,21 @@
         foreach ($lists as $list) {
             if (trim($list)) {
                 $list = explode('|', $list);
-                [$label, $relation, $field, $val, $sortKey, $alg, $limit, $template] = array_merge($list, [
-                    'Phim hot',
-                    '',
-                    'type',
-                    'series',
-                    'view_total',
-                    'desc',
-                    4,
-                    'top_thumb',
-                ]);
+                [$label, $relation, $field, $val, $sortKey, $alg, $limit, $template] = array_merge($list, ['Phim hot', '', 'type', 'series', 'view_total', 'desc', 4, 'top_thumb']);
                 try {
                     $data[] = [
                         'label' => $label,
                         'template' => $template,
-                        'data' => \Ophim\Core\Models\Movie::when($relation, function ($query) use (
-                            $relation,
-                            $field,
-                            $val,
-                        ) {
+                        'data' => \Ophim\Core\Models\Movie::when($relation, function ($query) use ($relation, $field, $val) {
                             $query->whereHas($relation, function ($rel) use ($field, $val) {
                                 $rel->where($field, $val);
                             });
+                        })->when(!$relation, function ($query) use ($field, $val) {
+                            $query->where($field, $val);
                         })
-                            ->when(!$relation, function ($query) use ($field, $val) {
-                                $query->where($field, $val);
-                            })
-                            ->orderBy($sortKey, $alg)
-                            ->limit($limit)
-                            ->get(),
+                        ->orderBy($sortKey, $alg)
+                        ->limit($limit)
+                        ->get(),
                     ];
                 } catch (\Exception $e) {
                     # code
